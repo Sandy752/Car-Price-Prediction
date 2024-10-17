b347dc9d19f37a95891b582f2a4423577b944151
sandy752 <sandeepkumaritm@gmail.com>
2024-10-17 14:57:32 +0530

    Added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,15 +3,13 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -25,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\cardekho_imputated.csv')
+            df=pd.read_csv(r'notebook/data/cardekho_imputated.csv', index_col=[0])
             logging.info('Read the dataset as dataframe')
 
             df.drop('car_name', axis=1, inplace=True)
